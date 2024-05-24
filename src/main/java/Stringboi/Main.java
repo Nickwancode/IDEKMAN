33b0d11a121144f695eb736e82de6b4607b07710
Nickwancode <simplynick0904@gmail.com>
2024-05-24 13:37:02 -0400

    MyTesterrr

---
+++ src/main/java/Stringboi/Main.java
@@ -0,0 +1,26 @@
+package Stringboi;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "jeff";
+        String named = "jeff";
+        String hi = new String();
+        hi = "jeff";
+        String hi2 = new String();
+        hi2 = "jeff";
+
+        System.out.println(name.equals(named));
+        System.out.println(hi2 == hi);
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+    }
+}
